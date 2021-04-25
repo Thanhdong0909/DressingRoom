b0a96c8bd20de44798f422389eda0bad65e98d52
dongpt-2023 <phan.thanh.dong@sun-asterisk.com>
2021-04-25 21:11:12 +0700

    first commit

---
+++ cyberlearn_dressingroom/controllers/main.js
@@ -0,0 +1,150 @@
+$(document).ready(() => {
+  let callData = new CallData();
+  let listChosen = new ListChosen();
+
+  callData.getListData()
+    .done((data)=> {
+      const {navPills, tabPanes} = data;
+      let contentHtml = '';
+      let contentTabName = '';
+      navPills.forEach((item, index) => {
+        const activeClass = index === 0 ? "active" : "";
+        const fadeClass = index !== 0 ? "fade" : "";
+        contentHtml += getElementTabName(item, activeClass);
+        contentTabName += `
+          <div class="tab-pane container ${fadeClass} ${activeClass}" id="${item.tabName}">
+            <div class="row">
+              ${renderTabpen(item.tabName, tabPanes)}
+            </div>
+          </div>
+        `
+      });
+      $(".nav-pills").html(contentHtml);
+      $(".tab-content").html(contentTabName);
+    })
+    .fail((err) => {
+      console.log(err);
+    });
+
+    const getElementTabName = (item, activeClass) => {
+      return `
+        <li class="nav-item">
+        <a
+        class="nav-link btn-default ${activeClass}"
+        data-toggle="pill"
+        href="#${item.tabName}"
+        >
+        ${item.showName}
+        </a>
+        </li>
+      `
+    }
+
+    const getListtabPanesByType = (type, tabPanes) => {
+      let result = [];
+      tabPanes.forEach((item)=>{
+        if(type === item.type) {
+          result.push(item);
+        }
+      })
+      return result;
+    }
+
+    const getElementContentByTab = (listContent) => {
+      let elementResult = '';
+      listContent.forEach((item)=> {
+        elementResult += `
+          <div class="col-md-3">
+            <div class="card text-center">
+            <img src="${item.imgSrc_jpg}">
+            <h4><b>${item.name}</b></h4>
+            <button
+              data-id="${item.id}"
+              data-type="${item.type}"
+              data-name="${item.name}"
+              data-desc="${item.desc}"
+              data-jpg="${item.imgSrc_jpg}"
+              data-png="${item.imgSrc_png}"
+              class="changeStyle"
+            >
+            Thử Đồ</button>
+            </div>
+          </div>
+        `
+      })
+      return elementResult;
+    }
+
+    const renderTabpen = (tabName, tabPanes) => {
+      let arrTemp = [];
+      let elementResult = '';
+      switch (tabName) {
+        case "tabTopClothes":
+          arrTemp = getListtabPanesByType("topclothes", tabPanes)
+          elementResult = getElementContentByTab(arrTemp)
+          break;
+        case "tabBotClothes":
+          arrTemp = getListtabPanesByType("botclothes", tabPanes)
+          elementResult = getElementContentByTab(arrTemp)
+          break;
+        case "tabShoes":
+          arrTemp = getListtabPanesByType("shoes", tabPanes)
+          elementResult = getElementContentByTab(arrTemp)
+          break;
+        case "tabHandBags":
+          arrTemp = getListtabPanesByType("handbags", tabPanes)
+          elementResult = getElementContentByTab(arrTemp)
+          break;
+        case "tabNecklaces":
+          arrTemp = getListtabPanesByType("necklaces", tabPanes)
+          elementResult = getElementContentByTab(arrTemp)
+          break;
+        case "tabHairStyle":
+          arrTemp = getListtabPanesByType("hairstyle", tabPanes)
+          elementResult = getElementContentByTab(arrTemp)
+          break;
+        default:
+          arrTemp = getListtabPanesByType("background", tabPanes)
+          elementResult = getElementContentByTab(arrTemp)
+          break;
+      }
+      return elementResult;
+    }
+
+     function findIndex(type) {
+      if (listChosen.arr && listChosen.arr.length > 0) {
+        // listChosen.arr.forEach((item, index) => {
+        //   if(item.type === type) {
+        //     debugger
+        //     return index;
+        //   }
+        // })
+        for(let i =0; i < listChosen.arr.length; i++){
+          if(listChosen.arr[i].type === type) {
+            return i;
+          }
+        }
+      }
+      return -1;
+    }
+
+    $('body').delegate(".changeStyle", "click", function(){
+      let id = $(this).data("id");
+      let type = $(this).data("type");
+      let name = $(this).data("name");
+      let desc = $(this).data("desc");
+      let imgsrc_jpg = $(this).data("jpg");
+      let imgsrc_png = $(this).data("png");
+
+      let chooseItem = new ChoseItem(id, type, name, desc, imgsrc_jpg, imgsrc_png);
+      // console.log("🚀 ~ file: main.js ~ line 140 ~ $ ~ chooseItem", chooseItem)
+
+      let index = findIndex(chooseItem.type);
+      if(index === -1) {
+        listChosen.addAddItem(chooseItem);
+      } else {
+        listChosen.arr[index] = chooseItem;
+      }
+      console.log("🚀 ~ file: main.js ~ line 121 ~ $ ~ chooseItem", listChosen.arr)
+    })
+})
